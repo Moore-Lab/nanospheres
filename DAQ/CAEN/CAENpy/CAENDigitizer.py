a3c5a0272c90e5476ccd516f2a284d8e00d55aa6
usphere lab <david.c.moore@yale.edu>
2024-04-26 15:37:54 -0400

    fix trigger level setup

--- DAQ/CAEN/CAENpy/CAENDigitizer.py
+++ DAQ/CAEN/CAENpy/CAENDigitizer.py
@@ -715,7 +715,7 @@ class CAEN_DT5740_Digitizer:
 		check_error_code(code)
 		return value.value
 
-	def setup_trigger(self, group, thresh, mode="ACQ_ONLY", channel_mask=0b11111111):
+	def setup_trigger(self, group, thresh, mode="ACQ_ONLY", channel_mask=0b01010101):
 		""" Setup the trigger
 
 		Arguments
