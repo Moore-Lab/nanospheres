714cf4932a3889e6a6366110e9d2cdb77986aab6
David Moore <david.c.moore@yale.edu>
2025-02-13 13:45:32 -0500

    analog input working

--- DAQ/LabJackT7/labjack_utils.py
+++ DAQ/LabJackT7/labjack_utils.py
@@ -19,7 +19,8 @@ class LabJack:
         self.deviceType = self.info[0]
 
 
-    def config(self, sample_rate=1000, num_samps = 100, channel_list=[0,1], voltage_range=10):
+    def config(self, sample_rate=1000, num_samps_to_read = 100, channel_list=[0,1], voltage_range=10,
+               stream_out=False, stream_out_channel=0, stream_out_data=[0]):
                 
         # Stream Configuration
         aScanListNames = []#["AIN0", "AIN1"]  # Scan list names to stream
@@ -28,7 +29,8 @@ class LabJack:
         numAddresses = len(channel_list)
         aScanList = ljm.namesToAddresses(numAddresses, aScanListNames)[0]
         scanRate = sample_rate
-        scansPerRead = int(num_samps)
+        scansPerRead = int(num_samps_to_read)
+        print("Scans per read: ", scansPerRead)
 
         self.numAddresses = numAddresses
 
@@ -77,8 +79,51 @@ class LabJack:
         numFrames = len(aNames)
         ljm.eWriteNames(self.handle, numFrames, aNames, aValues)
 
+        TOTAL_NUM_CHANNELS = numAddresses
+
+        if stream_out:
+
+
+            ljm.initializeAperiodicStreamOut(self.handle, 0, 3000, sample_rate)
+            # Write some data to the buffer before the stream starts
+            ljm.writeAperiodicStreamOut(self.handle, 0, len(stream_out_data), stream_out_data)
+            ljm.writeAperiodicStreamOut(self.handle, 0, len(stream_out_data), stream_out_data)  
+
+            #TOTAL_NUM_CHANNELS += 1
+            #STREAM_OUT_NAME = "STREAM_OUT0"
+            #STREAM_OUT_CHANNEL = "DAC" + str(stream_out_channel)
+            #NUM_SAMPLES = len(stream_out_data)
+            #ljm.eWriteName(self.handle, f"{STREAM_OUT_NAME}_TARGET", ljm.nameToAddress(STREAM_OUT_CHANNEL)[0])  # Set target as DAC0
+            #ljm.eWriteName(self.handle, f"{STREAM_OUT_NAME}_BUFFER_SIZE", 4*NUM_SAMPLES)  # Set buffer size
+            #ljm.eWriteName(self.handle, "STREAM_OUT0_ENABLE", 1)
+
+            # Load the buffer into STREAM_OUT0
+            #print("Loading ", stream_out_data)
+            #ljm.eWriteName(self.handle, "STREAM_OUT0_LOOP_SIZE", NUM_SAMPLES)
+            #for i in range(NUM_SAMPLES):
+            #    ljm.eWriteName(self.handle, f"{STREAM_OUT_NAME}_BUFFER_F32", stream_out_data[i])
+            #ljm.eWriteNameArray(self.handle, f"{STREAM_OUT_NAME}_BUFFER_F32", NUM_SAMPLES, stream_out_data.tolist())
+
+            # Write values to the stream-out buffer
+            #ljm.eWriteName(self.handle, "STREAM_OUT0_LOOP_SIZE", 6)
+            #ljm.eWriteName(self.handle, "STREAM_OUT0_BUFFER_F32", 0.0)  # 0.0 V
+            #ljm.eWriteName(self.handle, "STREAM_OUT0_BUFFER_F32", 1.0)  # 1.0 V
+            #ljm.eWriteName(self.handle, "STREAM_OUT0_BUFFER_F32", 2.0)  # 2.0 V
+            #ljm.eWriteName(self.handle, "STREAM_OUT0_BUFFER_F32", 3.0)  # 3.0 V
+            #ljm.eWriteName(self.handle, "STREAM_OUT0_BUFFER_F32", 4.0)  # 4.0 V
+            #ljm.eWriteName(self.handle, "STREAM_OUT0_BUFFER_F32", 5.0)  # 5.0 V
+
+            #ljm.eWriteName(self.handle, "STREAM_OUT0_SET_LOOP", 1)
+            #print("STREAM_OUT0_BUFFER_STATUS = %f" % (ljm.eReadName(self.handle, "STREAM_OUT0_BUFFER_STATUS")))
+
+
+
+            aScanList.extend(ljm.nameToAddress("STREAM_OUT0"))
+            TOTAL_NUM_CHANNELS += 1
+            #print(TOTAL_NUM_CHANNELS, aScanList)
+
         # Configure and start stream
-        scanRate = ljm.eStreamStart(self.handle, scansPerRead, numAddresses, aScanList, scanRate)
+        scanRate = ljm.eStreamStart(self.handle, scansPerRead, TOTAL_NUM_CHANNELS, aScanList, scanRate)
         print("\nStream started with a scan rate of %0.0f Hz." % scanRate)
 
     def close(self):
@@ -89,7 +134,7 @@ class LabJack:
         value = voltage 
         ljm.eWriteName(self.handle, name, value) 
 
-    def read_analog_in(self, num_scans=1):
+    def read_analog_in(self, num_scans=1, stream_out=False, stream_out_channel=0, stream_out_data=[0]):
 
         totScans = 0
         totSkip = 0
@@ -98,6 +143,10 @@ class LabJack:
         data_out = []
 
         while totScans < num_scans:
+
+            if(stream_out):
+                ljm.writeAperiodicStreamOut(self.handle, stream_out_channel, len(stream_out_data), stream_out_data)
+
 
             ret = ljm.eStreamRead(self.handle)
             aData = ret[0]
@@ -115,3 +164,27 @@ class LabJack:
             totScans += 1
 
         return data_out, totSkip
+    
+    def stream_analog_out(self, channel_list=[0], sample_rate=1000, writeData=[0]):
+
+        streamOutIndex = 0
+        scansPerRead = int(sample_rate/2)
+
+        # The desired stream channels
+        # Up to 4 out-streams can be ran at once
+        scanListNames = ["STREAM_OUT0"]
+        scanList = ljm.namesToAddresses(len(scanListNames), scanListNames)[0]
+
+        out_channel_addr = []
+        for channel in channel_list:
+            name = "DAC" + str(channel)
+            out_channel_addr.append(ljm.nameToAddress(name)[0])
+        print("streaming to channels: ", out_channel_addr)  
+
+        for targetAddr in out_channel_addr:
+            ljm.periodicStreamOut(self.handle, streamOutIndex, targetAddr, sample_rate, len(writeData), writeData)
+        
+        actualScanRate = ljm.eStreamStart(self.handle, scansPerRead, len(scanList), scanList, sample_rate)
+        print("Stream started with scan rate of %f Hz\n" % (sample_rate))
+
+        return actualScanRate
